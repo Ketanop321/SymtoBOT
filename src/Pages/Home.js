638ad3635ac8d866abc3ee70245a498cef92f709
Ketanop321 <ketanop473@gmail.com>
2024-11-02 20:00:49 +0530

    made the home page responsive

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,14 +5,11 @@ import Header from '../components/Header';
 const Home = () => {
   return (
     <div>
-      {/* <div className = "RightPage">
-        <Cards/>
-      </div> */}
+      
       <div className="LeftPage">
         <Header />
       </div>
-      {/* <img alt="" src={HealthImage} className="HealthImage" /> */}
-    </div>
+     </div>
   );
 };
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,48 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { Link } from 'react-router-dom';
+import './Navbar.css';  
 
 const Navbar = () => {
   return (
-    <nav style={styles.navbar}>
-      <ul style={styles.navList}>
-        <li style={styles.navItem}>
-          <Link to="/" style={styles.link}>Home</Link>
+    <nav className="navbar">
+      <ul className="navList">
+        <li className="navItem">
+          <Link to="/" className="link">Home</Link>
         </li>
-        <li style={styles.navItem}>
-          <Link to="/about" style={styles.link}>About Us</Link>
+        <li className="navItem">
+          <Link to="/about" className="link">About Us</Link>
         </li>
-        <li style={styles.navItem}>
-          <Link to="/contact" style={styles.link}>Contact</Link>
+        <li className="navItem">
+          <Link to="/contact" className="link">Contact</Link>
         </li>
-        <li style={styles.navItem}>
-          <Link to="/dashboard" style={styles.link}> COVID-Dashboard</Link>
+        <li className="navItem">
+          <Link to="/dashboard" className="link">COVID-Dashboard</Link>
         </li>
       </ul>
     </nav>
   );
 };
 
-const styles = {
-  navbar: {
-    background: "green",
-    padding: "15px",
-    display: "flex",
-    justifyContent: "center",  
-  },
-  navList: {
-    display: "flex",
-    listStyle: "none",
-    margin: 0,
-    padding: 0,
-  },
-  navItem: {
-    marginRight: "20px",
-  },
-  link: {
-    color: "#fff",
-    textDecoration: "none",
-    fontSize: "20px",
-  },
-};
-
-export default Navbar; 
+export default Navbar;
